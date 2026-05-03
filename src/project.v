b3acc2427468b0f653be439cbbcc9f416a41011c
Gurtejbir Randhawa <gurtejbirsr@gmail.com>
2026-05-03 10:19:57 -0600

    fourth commit

--- src/project.v
+++ src/project.v
@@ -6,7 +6,7 @@
 
 `default_nettype none
 
-module tt_um_vga_gurtej-randhawa1_Gurtej-Eshaan-Tapeout(
+module tt_um_vga_gurtej_randhawa1_Gurtej_Eshaan_Tapeout(
   input  wire [7:0] ui_in,    // Dedicated inputs
   output wire [7:0] uo_out,   // Dedicated outputs
   input  wire [7:0] uio_in,   // IOs: Input path
